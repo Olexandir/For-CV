7601b22d04e348b4bfa9eca917f9b30eb99896b6
Olexandir <68996102+Olexandir@users.noreply.github.com>
2021-02-11 12:50:19 +0300

    Оставил только папку с пет-проектами

--- CSS Library/Accordeon/script.js
+++
@@ -1,7 +0,0 @@
-const accordion = document.getElementById("accordion");
-
-for (let i of accordion.children) {
-  i.addEventListener("click", (e) => {
-    i.classList.toggle("expand");
-  });
-}
--- Projects to become skilled/Act I/Background changer/script.js
+++
@@ -1,9 +0,0 @@
-const btn = document.getElementById("btn");
-
-btn.addEventListener("click", () => {
-  document.body.style.background = changeMe();
-});
-
-function changeMe() {
-  return `hsl(${Math.floor(Math.random() * 360)}, 100%, 50%)`;
-}
