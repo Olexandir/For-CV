25f741da3d22527be95a785af8977e57d99923ac
Olexandir <68996102+Olexandir@users.noreply.github.com>
2021-02-23 20:37:49 +0300

    adding modules & end the project

---
+++ Pet projects/Burger menu/Menu.js
@@ -0,0 +1,20 @@
+class Menu {
+    constructor(options){
+this.button = options.button;
+this.menu = options.menu;
+    }
+
+clickHandler(){
+    this.button.addEventListener('click', (e) =>{
+        this.menu.classList.add('active');
+
+    })
+    this.menu.addEventListener('click', (e) =>{
+        this.menu.classList.remove('active');
+
+    })
+}
+
+}
+
+export {Menu}
---
+++ Pet projects/Burger menu/script.js
@@ -0,0 +1,11 @@
+import {Menu} from './Menu.js'
+
+const btn = document.querySelector('.btn-wrapper, .grey, .btn');
+const mn = document.querySelector('.menu-wrapper');
+
+const menu = new Menu({
+    button: btn,
+    menu: mn,
+})
+
+menu.clickHandler()
